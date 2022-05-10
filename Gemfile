1e59e2f200f7c6ac446962f5f4776075d2f290fc
David Caldera <davidcaldera13@yahoo.com>
2022-05-10 11:37:34 -0500

    change cors, add login, signup, logout, sessions

--- Gemfile
+++ Gemfile
@@ -51,3 +51,5 @@ end
 gem "active_model_serializers", "~> 0.10.12"
 
 gem "faker"
+
+gem 'rack-cors'
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,15 @@ Rails.application.routes.draw do
   resources :users
 
   get "/posts/:id/comments", to: "posts#index_comments"
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
+
+  # Routing logic: fallback requests for React Router.vvvv
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  # Leave this here to help deploy your app later!^^^^
+
+
+
+  get "/me", to: "users#show" #make useEffect in app to fetch here to see if logged in
+  post "/signup", to: "users#create"
+  post '/login', to:  'sessions#create' 
+  delete 'logout', to:  'sessions#destroy'
 end
